a086cd78f90c80de91ab1c3b9e5197b42ad0f702
GreatWei <1121412775@qq.com>
2018-10-25 01:03:44 +0800

    Signed-off-by: GreatWei <1121412775@qq.com>

--- src/algs4/Ten.java
+++ src/algs4/Ten.java
@@ -1,5 +1,7 @@
 package algs4;
 
+import edu.princeton.cs.algs4.Shell;
+
 public class Ten {
 
 	public static void main(String[] args) {
@@ -7,4 +9,30 @@ public class Ten {
 
 	}
 
+	// 希尔排序
+	public static int[] shellSort(int arr[]) {
+		int len = arr.length;
+		int gap = 1;
+		int tmp;
+		
+		// 最大区间划分
+		while (gap < len / 3) {
+			gap = gap * 3 + 1;
+
+		}
+		while (gap > 0) {
+			for (int i = gap; i < len; i++) {
+				int j = i - gap;
+				int temp = arr[i];
+				for (; j > 0 && arr[j] > temp; j =j- gap) {
+					arr[j + gap] = arr[j];
+				}
+				arr[j + gap] = temp;
+			}
+			gap = (gap - 1) / 3;
+
+		}
+		return arr;
+
+	}
 }
